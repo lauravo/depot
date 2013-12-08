17ac2433b00d1013cbb8f3abbb06af2d61bc4f03
Laura Vo <laura.g.vo@gmail.com>
2013-12-07 20:33:17 -0800

    ddddq dddd#	new file:   tmp/cache/assets/CB4/250/sprockets%2F2d09424cb334c447f9792b5331f6da86

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'bcd4ea401f5b576006fbce4078829f78456ee45fe9b68b3f604b50e71f0d79dd13c09f7f77a627639d8370c24954c3cf328d79d8fb431b457f1c942f76ce79cb'
